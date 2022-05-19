7fdf3bcf14e9f3677bca52f94d7b0274e0667bb4
Stephanie Guzman <sguzman247@gmail.com>
2022-05-19 07:54:10 -0500

    Complete dragon test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,52 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.meals = 0
+  }
 
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+
+//return false boolean at 4 +
+//track # of times it eats > once count reaches 3 > reassign this.hungry to False
+  eat() {
+    ++this.meals
+    if (this.meals >= 3) {
+      this.hungry = false;
+    }
+  }
+}
+
+
+
+module.exports = Dragon;
+
+
+
+// use decrement -- operator , use --x returns decrement value AFTER decrementing
+
+  //     {
+  //   if (this.meals <= 3) {
+  //     return this.hungry = true
+  //   } else {
+  //     return this.hungry = false
+  //   }
+  // }
+    // for (var i = 0; i < this.meals.length; i++) {
+    //   return
+    //   if (this.meals < 4) {
+    //     return this.hungry = true
+    //   } else {
+    //     return this.hungry = false
+    //   }
+    // }
+    //
+    // this.meals
+// }
+    //   if (this.meals < 4) {
+    //   return true
+    // } else {
+    //   return false
